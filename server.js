e0c524a66594cf7594b3bb6763e6649af049dc75
engistudy <geraldgonzaless123@gmail.com>
2026-05-04 11:52:48 +0800

    Prepare portfolio for Render deployment

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.post("/send-email", async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: "egijoefranmorales123@gmail.com",
+        pass: "gwpuhdreotnpkmas",
       },
     });
 
