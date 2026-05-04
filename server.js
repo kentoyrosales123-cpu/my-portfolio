7b31a92154a6c5c77b68519bb167f17567ed0271
engistudy <geraldgonzaless123@gmail.com>
2026-05-04 11:33:37 +0800

    Prepare portfolio for Render deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
